b234ded3f5bfe4113c19c1591a31e238f1b44c95
MelroyL92 <melroy_lingsma@live.nl>
2023-11-08 12:36:28 +0100

    Made a more dynamic Navlinks class

--- src/components/Navlinks/Navlinks.jsx
+++ src/components/Navlinks/Navlinks.jsx
@@ -2,43 +2,27 @@ import {Link} from "react-router-dom"
 import './Navlinks.css'
 
 
-function NavLinks (){
+function NavLinks ({ to, iconSrc, altText, text }){
 
     return (
         <div className="nav-class">
-        <nav>
-                 <ul className="nav-list-item">
-                   <div className="list-class"><li>logo</li></div>
-                     <li><Link to="/">
+            <ul>
+                <li className="nav-list-container">
+                    <Link className="Link-style" to={to}>
                         <div className="list-class">
-                        <span className="icon-wrapper"><img src="src/assets/house-line-thin.svg" alt="home icon"/></span>
-                       Home</div>
-                        </Link></li>
-                    <li><Link to="/WishlistOverview">
-                        <div className="list-class">
-                         <span className="icon-wrapper"><img src="src/assets/list-thin.svg" alt="wishlist icon"/></span>
-                        wishlists</div>
-                    </Link></li>
-                    <li><Link to="/SearchResultPage">
-                        <div className="list-class">
-                         <span className="icon-wrapper"><img src="src/assets/magnifying-glass-thin.svg" alt="magnifying glass icon"/></span>
-                        Search</div></Link></li>
-                    <li><Link to="/About">
-                        <div className="list-class">
-                         <span className="icon-wrapper"><img src="src/assets/info-thin.svg" alt="about icon"/></span>
-                        About</div></Link></li>
-                    <li><Link to="/Login">
-                        <div className="list-class">
-                         <span className="icon-wrapper"><img src="src/assets/user-thin.svg" alt="login icon"/></span>
-                        Login</div></Link></li>
-                    <li><Link to="/Register">
-                        <div className="list-class">
-                         <span className="icon-wrapper"><img src="src/assets/alien-thin.svg" alt="register icon"/></span>
-                        Register</div></Link></li>
-                </ul>
-        </nav>
+                            {text}
+                            <span className="icon-wrapper">
+                                <img src={iconSrc} alt={altText} />
+                            </span>
+                        </div>
+                    </Link>
+                </li>
+            </ul>
         </div>
-    )
+    );
 }
+
+
+
 
 export default NavLinks
