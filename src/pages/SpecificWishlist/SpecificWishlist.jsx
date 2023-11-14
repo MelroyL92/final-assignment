3a4bdd551b8ba74dd58b4ea11c769f3dfa371013
MelroyL92 <melroy_lingsma@live.nl>
2023-11-14 21:13:58 +0100

    Comment and grade section + navbar works for the specific wishlist

--- src/pages/SpecificWishlist/SpecificWishlist.jsx
+++ src/pages/SpecificWishlist/SpecificWishlist.jsx
@@ -9,35 +9,23 @@ import Button from "../../components/Forms/Button/Button.jsx";
 import Input from "../../components/Forms/Input/Input.jsx";
 import {useForm} from "react-hook-form";
 
-
 function SpecificWishlist() {
     const { listName } = useParams();
-    const {wishlist} = useContext(WishlistContext);
-    const {register, handleSubmit, formState: {errors}} = useForm();
-    const [grade,setGrade] = useState(0)
-    const [specificListSearch, setSpecificListSearch] = useState(null);
+    const { wishlist } = useContext(WishlistContext);
+    const { register, handleSubmit, formState: { errors }, setValue, getValues } = useForm();
+    const [grade, setGrade] = useState(0);
     const [filterSearch, setFilterSearch] = useState('');
+    const [editMode, setEditMode] = useState({});
+    const [commentsAndGrades, setCommentsAndGrades] = useState({}); // Define commentsAndGrades state
 
-    useEffect(() => {
-        const foundList = wishlist.find(
-            (list) => list.name.toLowerCase() === listName.toLowerCase()
-        );
-        setSpecificListSearch(foundList);
-    }, [wishlist, listName]);
 
-    const handleSearch = (searchTerm) => {
-        setFilterSearch(searchTerm);
+    //logic for the comments for the specific wishlist, similar to the overview
+    const handleEditToggle = (gameId) => {
+        setEditMode((prevEditMode) => ({
+            ...prevEditMode,
+            [gameId]: !prevEditMode[gameId],
+        }));
     };
-
-    // Filter games based on the search term
-    const filteredGames = specificListSearch
-        ? specificListSearch.games.filter((game) =>
-            game.name.toLowerCase().includes(filterSearch.toLowerCase())
-        )
-        : [];
-
-
-
 
     const handleSliderChange = (event, gameId) => {
         setGrade((prevGrade) => ({
@@ -46,86 +34,115 @@ function SpecificWishlist() {
         }));
     };
 
+    const handleSpecificWishlistSubmit = (gameId) => {
+        // Your logic to handle form submission
+        const updatedCommentsAndGrades = {
+            ...commentsAndGrades,
+            [gameId]: {
+                comment: getValues(`comment-${gameId}`),
+                grade: grade[gameId] || 0,
+            },
+        };
 
+        setCommentsAndGrades(updatedCommentsAndGrades);
+        localStorage.setItem(`wishlist-comment-${listName}`, JSON.stringify(updatedCommentsAndGrades));
+        alert('Your comments have been saved');
 
+        // Toggle off edit mode after submitting
+        handleEditToggle(gameId);
+    };
 
-    // should store the data of the comments in local storage but doesn't work yet
     useEffect(() => {
+        const storedData = JSON.parse(localStorage.getItem(`wishlist-comment-${listName}`)) || {};
+        setCommentsAndGrades(storedData);
+    }, [listName]);
 
-        const storedData = localStorage.getItem('wishlist comment') // Hoe moet ik er nu voor zorgen dat het
-        console.log(storedData)
-
-    }, [])
-
-    // should store the data of the comments in local storage but doesn't work yet
-    useEffect(() => {
-
-        const storedData = localStorage.getItem('wishlist comment') // Hoe moet ik er nu voor zorgen dat het
-        console.log(storedData)
-
-    }, [])
-
-
-    // Filter the specific list by name
-    const specificList = wishlist.find((titleList) => titleList.name === listName);
+    const specificList = wishlist.find(
+        (list) => list.name.toLowerCase() === listName.toLowerCase()
+    );
 
     if (!specificList) {
-        // Handle the case where the specific list is not found
         return <div>List not found</div>;
     }
 
+    // filters the game based on grade or name
+    const filteredGames = specificList.games.filter((game) =>
+        game.name.toLowerCase().includes(filterSearch.toLowerCase()) ||
+        (commentsAndGrades[game.id]?.grade && commentsAndGrades[game.id]?.grade.toString() === filterSearch)
+    );
+    console.log(filteredGames)
 
-    function handleSpecificWishlistSubmit(data) {
-        console.log(data);
-        localStorage.setItem(specificList.name , JSON.stringify(data))
+    const handleSearch = (searchTerm) => {
+        setFilterSearch(searchTerm);
+    };
 
-        alert('Your comments have been saved');
-        // You can call handleSearch or any other function with the form data here
-    }
 
 
     return (
         <main className="parent-container">
             <nav className="nav-class">
-            <NavLinks to="/" iconSrc="../src/assets/house-line-thin.svg" altText="home icon" text="Home" />
-            <NavLinks to="/WishlistOverview" iconSrc="../src/assets/list-thin.svg" altText="wishlist icon" text="Wishlists" />
-            <NavLinks to="/SearchResultPage" iconSrc="../src/assets/magnifying-glass-thin.svg" altText="magnifying glass icon" text="Search" />
-            <NavLinks to="/About" iconSrc="../src/assets/info-thin.svg" altText="about icon" text="About" />
-            <NavLinks to="/Login" iconSrc="../src/assets/user-thin.svg" altText="login icon" text="Login" />
-            <NavLinks to="/Register" iconSrc="../src/assets/alien-thin.svg" altText="register icon" text="Register" />
-        </nav>
+                <NavLinks to="/" iconSrc="../src/assets/house-line-thin.svg" altText="home icon" text="Home"/>
+                <NavLinks to="/WishlistOverview" iconSrc="../src/assets/list-thin.svg" altText="wishlist icon"
+                          text="Wishlists"/>
+                <NavLinks to="/SearchResultPage" iconSrc="../src/assets/magnifying-glass-thin.svg"
+                          altText="magnifying glass icon" text="Search"/>
+                <NavLinks to="/About" iconSrc="../src/assets/info-thin.svg" altText="about icon" text="About"/>
+                <NavLinks to="/Login" iconSrc="../src/assets/user-thin.svg" altText="login icon" text="Login"/>
+                <NavLinks to="/Register" iconSrc="../src/assets/alien-thin.svg" altText="register icon"
+                          text="Register"/>
+            </nav>
             <div className="page-wrapper">
                 <div className="head-container">
-                    <h1>{specificList.name}</h1>
-                    <SearchBar source="../src/assets/rawg-logo_750x430.jpg"
-                               clickHandler={(event) => setFilterSearch(event.target.value)}
-                               onSearch={handleSearch}
+                    <h1>{filteredGames.name}</h1>
+                    <SearchBar
+                        source="../src/assets/rawg-logo_750x430.jpg"
+                        clickHandler={(event) => setFilterSearch(event.target.value)}
+                        onSearch={handleSearch}
                     />
                 </div>
                 <ul>
                     {filteredGames.map((game) => (
                         <li key={game.id} className="wishlist-overview-wrapper">
-            <span>
-              <img src={game.background_image} alt="game-image" />
-            </span>
+                            <span>
+                                <img src={game.background_image} alt="game-image" />
+                            </span>
                             <div>
                                 <Link to={`/GameDetail/${game.id}`}>{game.name}</Link>
                             </div>
-                            <form onSubmit={handleSubmit( () => handleSpecificWishlistSubmit(game.id, grade[game.id]))}>
-                                <Input
-                                inputName="comment"
-                                inputId="comment-field"
-                                inputLabel="Comments"
-                                inputType="textarea"
-                                validationRules={{
-                                    required: false,
-                                }}
-                                register={register}
-                                errors={errors}
-                                />
-                                <Grade id={game.id} type="range" maxRange="10" minRange="0" changeHandler={(event) => handleSliderChange(event, game.id)} grade={grade[game.id] || 0}/>
-                                <Button type="submit" label="Grade"/>
-                            </form>
+                            {editMode[game.id] ? (
+                                <form onSubmit={handleSubmit(() => handleSpecificWishlistSubmit(game.id))}>
+                                    <Input
+                                        inputName={`comment-${game.id}`}
+                                        inputId={`comment-field-${game.id}`}
+                                        inputLabel="Comments"
+                                        inputType="textarea"
+                                        defaultValue={commentsAndGrades[game.id]?.comment || ''}
+                                        validationRules={{
+                                            required: false,
+                                        }}
+                                        register={register}
+                                        setValue={setValue}
+                                        errors={errors}
+                                    />
+                                    <Grade
+                                        id={game.id}
+                                        type="range"
+                                        maxRange="10"
+                                        minRange="0"
+                                        changeHandler={(event) => handleSliderChange(event, game.id)}
+                                        grade={grade[game.id] || 0}
+                                    />
+                                    <Button type="submit" label="Store Data" />
+                                </form>
+                            ) : (
+                                <>
+                                    <div>Comments: {commentsAndGrades[game.id]?.comment || 'No comment'}</div>
+                                    <div>Grade: {commentsAndGrades[game.id]?.grade || 'No grade'}</div>
+                                </>
+                            )}
+                            <button onClick={() => handleEditToggle(game.id)}>
+                                {editMode[game.id] ? 'Cancel' : 'Edit'}
+                            </button>
                         </li>
                     ))}
                 </ul>
@@ -134,4 +151,9 @@ function SpecificWishlist() {
     );
 }
 
-export default  SpecificWishlist
+export default SpecificWishlist;
+
+
+
+
+
