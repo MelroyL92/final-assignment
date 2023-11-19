571ed43cb716f92746855b94fcb651ad682f833a
MelroyL92 <melroy_lingsma@live.nl>
2023-11-19 15:01:35 +0100

    Small updates to the style

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -37,7 +37,7 @@ function About () {
                     <p>This is a project designed as a result of a frontend assignment by the NOVI hogeschool.</p>
                     <p>Studied there for a few months before starting this assignment and spend about 3 weeks of work to get this done</p>
 
-                    <h2>About the website</h2>
+                    <h3>The website</h3>
                     <p>This website was created as a test to see what i could do but the purpose of the website remains as follows:</p>
                     <p>The purpose has been to create a website for people to organise their games.To be able to create lists so that they could figure out a few things like:</p>
                     <ul>
@@ -46,7 +46,6 @@ function About () {
                         <li>What grade would i give this game</li>
                     </ul>
                     <p>With that in mind it might be easier to go down a list of games you would like to play without having to have doubts about it</p>
-                    <p>I hope you enjoy this simple website!</p>
                     <h4>Keep in mind to delete your local storage when completely done as this website does not have a backend (yet)</h4>
                 </div>
             </div>
